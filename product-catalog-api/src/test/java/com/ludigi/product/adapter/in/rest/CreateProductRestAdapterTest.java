ad6994bb40ec3870a8ca3ed5b59d50a4bdd399a4
Sławek Ludwiczak <slawek.ludwiczak@gmail.com>
2024-06-20 14:37:14 +0200

    works without keycloak

--- product-catalog-api/src/test/java/com/ludigi/product/adapter/in/rest/CreateProductRestAdapterTest.java
+++ product-catalog-api/src/test/java/com/ludigi/product/adapter/in/rest/CreateProductRestAdapterTest.java
@@ -10,6 +10,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
 
 import java.util.UUID;
@@ -25,6 +26,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
         webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT
 )
 @AutoConfigureMockMvc
+@ActiveProfiles("test")
 class CreateProductRestAdapterTest {
     @MockBean
     ProductCommandPort productCommandPort;
--- product-catalog-api/src/test/java/com/ludigi/product/adapter/in/rest/ProductCatalogApiApplication.java
+++ product-catalog-api/src/test/java/com/ludigi/product/adapter/in/rest/ProductCatalogApiApplication.java
@@ -1,10 +1,8 @@
 package com.ludigi.product.adapter.in.rest;
 
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.PropertySource;
 
 @SpringBootApplication
-@PropertySource("classpath:api.yml")
 class ProductCatalogApiApplication {
-    
+
 }
