f3f70000796161d11f7081e9a39c789e66dc341e
Slawek Ludwiczak <slawek.ludwiczak@gmail.com>
2024-06-26 11:57:28 +0200

    added consul, moved files to subdirectory

--- product-catalog/product-catalog-bootstrap/build.gradle.kts
+++ product-catalog/product-catalog-bootstrap/build.gradle.kts
@@ -6,13 +6,24 @@ plugins {
 
 apply(from = rootProject.file("buildSrc/shared.gradle.kts"))
 
+extra["springCloudVersion"] = "2023.0.2"
+
+dependencyManagement {
+	imports {
+		mavenBom("org.springframework.cloud:spring-cloud-dependencies:${property("springCloudVersion")}")
+	}
+}
+
 dependencies {
     implementation(project(":product-catalog-domain"))
     implementation(project(":product-catalog-api"))
     implementation(project(":product-catalog-persistence"))
+    implementation(project(":product-catalog-consul"))
     implementation (libs.spring.boot.starter)
     testImplementation (libs.spring.boot.starter.test)
     testRuntimeOnly(libs.junit.platform.launcher)
+    implementation(platform(org.springframework.boot.gradle.plugin.SpringBootPlugin.BOM_COORDINATES))
+    implementation("org.springframework.cloud:spring-cloud-starter-consul-discovery")
 }
 
 tasks.test {
---
+++ product-catalog/product-catalog-consul/src/test/java/com/ludigi/productcatalogservicediscovery/ProductCatalogServiceDiscoveryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ludigi.productcatalogservicediscovery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductCatalogServiceDiscoveryApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
