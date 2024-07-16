275d9ff493c23b7368f2e92c446018ea877a0b53
Sławek Ludwiczak <slawek.ludwiczak@gmail.com>
2024-07-16 14:20:32 +0200

    logging with loki

--- frontend-htmx/src/main/java/com/ludigi/priceflow/frontend/home/HomeController.java
+++ frontend-htmx/src/main/java/com/ludigi/priceflow/frontend/home/HomeController.java
@@ -1,18 +1,20 @@
 package com.ludigi.priceflow.frontend.home;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.MediaType;
 import org.springframework.security.oauth2.client.OAuth2AuthorizedClient;
 import org.springframework.security.oauth2.client.annotation.RegisteredOAuth2AuthorizedClient;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class HomeController {
+    private final Logger logger = LoggerFactory.getLogger(HomeController.class);
 
     @GetMapping(value = "/", produces = MediaType.TEXT_HTML_VALUE)
-    String home(@RegisteredOAuth2AuthorizedClient("keycloak") OAuth2AuthorizedClient authorizedClient,
-                Model model) {
+    String home(@RegisteredOAuth2AuthorizedClient("keycloak") OAuth2AuthorizedClient authorizedClient) {
+        logger.info(authorizedClient.getPrincipalName() + " viewing homepage");
         return "index";
     }
 }
