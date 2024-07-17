494133b9bdc3ef1ab0a0df7f7119aa3c5e7907fc
Sławek Ludwiczak <slawek.ludwiczak@gmail.com>
2024-07-17 10:48:21 +0200

    added loki for logging

--- frontend-htmx/src/main/java/com/ludigi/priceflow/frontend/home/HomeController.java
+++ frontend-htmx/src/main/java/com/ludigi/priceflow/frontend/home/HomeController.java
@@ -2,12 +2,16 @@ package com.ludigi.priceflow.frontend.home;
 
 import io.micrometer.core.instrument.Counter;
 import io.micrometer.core.instrument.MeterRegistry;
+import jakarta.servlet.http.HttpServletRequest;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class HomeController {
+    private final Logger LOG = LoggerFactory.getLogger(HomeController.class);
     private final Counter counter;
 
     public HomeController(MeterRegistry meterRegistry) {
@@ -15,8 +19,9 @@ public class HomeController {
     }
 
     @GetMapping(value = "/", produces = MediaType.TEXT_HTML_VALUE)
-    String home() {
+    String home(HttpServletRequest request) {
         counter.increment();
+        LOG.info("Homepage viewed by " + request.getRemoteAddr());
         return "index";
     }
 }
