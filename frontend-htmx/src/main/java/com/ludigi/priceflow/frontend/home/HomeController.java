6973c4d08c05a89f0763163dd603bf2ef97a200b
Sławek Ludwiczak <slawek.ludwiczak@gmail.com>
2024-07-25 13:02:31 +0200

    removed unnecessary logging

--- frontend-htmx/src/main/java/com/ludigi/priceflow/frontend/home/HomeController.java
+++ frontend-htmx/src/main/java/com/ludigi/priceflow/frontend/home/HomeController.java
@@ -1,27 +1,13 @@
 package com.ludigi.priceflow.frontend.home;
 
-import io.micrometer.core.instrument.Counter;
-import io.micrometer.core.instrument.MeterRegistry;
-import jakarta.servlet.http.HttpServletRequest;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 class HomeController {
-    private final Logger LOG = LoggerFactory.getLogger(HomeController.class);
-    private final Counter counter;
-
-    public HomeController(MeterRegistry meterRegistry) {
-        this.counter = Counter.builder("views").register(meterRegistry);
-    }
-
     @GetMapping(value = "/", produces = MediaType.TEXT_HTML_VALUE)
-    String home(HttpServletRequest request) {
-        counter.increment();
-        LOG.info("Homepage viewed by " + request.getRemoteAddr());
+    String home() {
         return "index";
     }
 }
